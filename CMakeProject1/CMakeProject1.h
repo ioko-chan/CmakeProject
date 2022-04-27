480dcf9f87396f6285e5a2e7cf6f6c7dee2d4f9d
Ekaterina <solomennicova555@gmail.com>
2022-04-27 12:27:38 +0200

    glad lib added

--- CMakeProject1/CMakeProject1.h
+++ CMakeProject1/CMakeProject1.h
@@ -3,6 +3,9 @@
 
 #pragma once
 
+#define GLFW_INCLUDE_NONE
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 // TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
